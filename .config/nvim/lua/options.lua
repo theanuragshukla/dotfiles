49de6fedda2e99cb65de80fd117121878373dc5c
theanuragshukla <tachyonanurag@gmail.com>
2024-05-06 15:18:37 +0530

    ⚡added nvim config

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,60 @@
+-- Set highlight on search
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+
+vim.opt.scrolloff = 8
+-- Make line numbers default
+vim.o.number = true
+vim.o.relativenumber = true
+
+-- Disable mouse mode
+vim.o.mouse = ''
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Decrease update time
+vim.o.updatetime = 250
+vim.wo.signcolumn = 'yes'
+
+-- Set colorscheme
+--vim.cmd [[colorscheme onedark]]
+vim.cmd.colorscheme "catppuccin"
+
+--vim.cmd()
+vim.opt.clipboard = 'unnamedplus'
+
+-- Set completeopt to have a better completion experience
+
+vim.o.completeopt = 'menuone,noselect'
+
+-- Concealer for Neorg
+vim.o.conceallevel=2
+
+-- [[ Basic Keymaps ]]
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
