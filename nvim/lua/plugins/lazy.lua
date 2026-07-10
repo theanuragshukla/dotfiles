06fe8947f43400ef7cefd041cd0dd7fb34223d6b
Anurag Shukla <tachyonanurag@gmail.com>
2026-07-10 14:31:04 +0530

    chore: flamshot, nvim-trouble

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -100,13 +100,7 @@ require('lazy').setup({
 	  {
     "folke/trouble.nvim",
     dependencies = "nvim-tree/nvim-web-devicons",
-    config = function()
-      require("trouble").setup {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      }
-    end
+		opts = {},
   },
 
   'mbbill/undotree', 
